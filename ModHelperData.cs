c521b5c8090fc01bc66179f36732b0ba5f400e1a
mharb <90333698+mharbb@users.noreply.github.com>
2025-08-01 01:04:49 -0700

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@ namespace FullScreenBorderless;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "49.0";
+    public const string WorksOnVersion = "49.X";
     public const string Version = "1.0.1";
     public const string Name = "FullScreenBorderless";
 
